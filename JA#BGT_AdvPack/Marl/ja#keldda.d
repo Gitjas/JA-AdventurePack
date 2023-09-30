7695f2a9bc365446107301b4f8c0435f4287d429
TotoR115 <81795188+TotoR115@users.noreply.github.com>
2023-10-01 00:38:32 +0300

    Marl
    - Remove Guards that was not directly linked to this component
    - Indentation

--- JA#BGT_AdvPack/Marl/ja#keldda.d
+++ JA#BGT_AdvPack/Marl/ja#keldda.d
@@ -45,23 +45,3 @@ EraseJournalEntry(%kelddath7%)
 TakePartyItem("MISC04")~
 END
 
-
-//Dunkin
-
-ALTER_TRANS DUNKIN
-BEGIN 7 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~ClearAllActions()~
-END
-
-//misc.d
-REPLACE_STATE_TRIGGER DUNKIN 5 ~Global("JA#DUNKIN","%Beregost_FeldepostsInn_L1%",1)~
-
-ALTER_TRANS DUNKIN // file name
-BEGIN 5 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#DUNKIN","%Beregost_FeldepostsInn_L1%",2)~
-END
-
