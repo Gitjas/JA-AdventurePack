160dd3d7a47663b84b5b8228b1b6f37e9995e7b9
TotoR115 <81795188+TotoR115@users.noreply.github.com>
2023-09-30 23:42:46 +0300

    Arkhstaff
    - Typo
    - clean-up

--- JA#BGT_AdvPack/Arkhstaff/ja#thaldo.d
+++ JA#BGT_AdvPack/Arkhstaff/ja#thaldo.d
@@ -1,12 +1,5 @@
 ALTER_TRANS THALDO // file name
-BEGIN 2 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#SAVETHALDO","GLOBAL",1)EscapeAreaDestroy(90)~
-END
-
-ALTER_TRANS THALDO // file name
-BEGIN 3 END // state number (can be more than one)
+BEGIN 2 3 END // state number (can be more than one)
 BEGIN 1 END // transition number (can be more than one)
 BEGIN // list of changes, see below for flags
   "ACTION" ~SetGlobal("JA#SAVETHALDO","GLOBAL",1)EscapeAreaDestroy(90)~
@@ -39,11 +32,3 @@ END
 EXTEND_BOTTOM THALDO 6
 IF ~~ THEN REPLY @0 EXIT
 END
-
-
-
-
-
-
-
-
